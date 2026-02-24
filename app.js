fae12db5e8e04da68232ff5df2a38507c562cebe
Froggles <elmsterm@gmail.com>
2026-02-24 10:51:26 -0500

    route express app

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 import express from "express";
 const app = express();
 export default app;
+
+import tracksRouter from "./api/tracks.js";
+import playlistsRouter from "./api/playlists.js";
+
+app.use(express.json());
+
+app.use("/tracks", tracksRouter);
+
+app.use("/playlists", playlistsRouter);
