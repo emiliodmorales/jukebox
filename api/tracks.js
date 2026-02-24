e24afabda0a354d4f6dc3f0386d42ca99f00a74f
Froggles <elmsterm@gmail.com>
2026-02-24 10:51:34 -0500

    create /tracks router

---
+++ api/tracks.js
@@ -0,0 +1,21 @@
+import { getTrack, getTracks } from "#db/queries/tracks";
+import { Router } from "express";
+const router = Router();
+export default router;
+
+router.get("/", async (req, res) => {
+  const tracks = await getTracks();
+  res.send(tracks);
+});
+
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  if (!id) return res.status(404).send("Track not found.");
+  if (Number.isNaN(+id))
+    return res.status(400).send("Track id must be a number.");
+
+  const track = await getTrack(id);
+  if (!track) return res.status(404).send("Track not found.");
+
+  res.send(track);
+});
