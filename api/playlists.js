55ab3b44a87a1f0894fa9d3034f9ef78156d7011
Froggles <elmsterm@gmail.com>
2026-02-24 10:56:30 -0500

    create playlist routes to get playlists

--- api/playlists.js
+++ api/playlists.js
@@ -1,3 +1,25 @@
+import { getPlaylist, getPlaylists } from "#db/queries/playlists";
 import { Router } from "express";
 const router = Router();
 export default router;
+
+router.get("/", async (req, res) => {
+  const playlists = await getPlaylists();
+  res.send(playlists);
+});
+
+router.param("id", async (req, res, next) => {
+  const { id } = req.params;
+  if (Number.isNaN(+id))
+    return res.status(400).send("Playlist id must be a number.");
+
+  const playlist = await getPlaylist(id);
+  if (!playlist) return res.status(404).send("Playlist not found.");
+
+  req.playlist = playlist;
+  next();
+});
+
+router.get("/:id", async (req, res) => {
+  res.send(req.playlist);
+});
