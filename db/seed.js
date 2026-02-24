2c8404814ba01dd9cf32d4a1c951ba6f72a3c1b1
Froggles <elmsterm@gmail.com>
2026-02-24 10:35:26 -0500

    create function to seed the database

--- db/seed.js
+++ db/seed.js
@@ -1,4 +1,7 @@
 import db from "#db/client";
+import { createPlaylist } from "./queries/playlists.js";
+import { addTrackToPlaylist } from "./queries/playlists_tracks.js";
+import { createTrack } from "./queries/tracks.js";
 
 await db.connect();
 await seed();
@@ -6,5 +9,20 @@ await db.end();
 console.log("🌱 Database seeded.");
 
 async function seed() {
-  // TODO
+  for (let i = 1; i <= 10; i++) {
+    await createPlaylist({
+      name: "Playlist " + i,
+      description: "blah blah blah",
+    });
+  }
+
+  for (let i = 1; i <= 20; i++) {
+    const track = await createTrack({
+      name: "Track " + i,
+      durationMs: Math.floor(Math.random() * 120000),
+    });
+
+    const playlistId = Math.floor(Math.random() * 10) + 1;
+    await addTrackToPlaylist(playlistId, track.id);
+  }
 }
